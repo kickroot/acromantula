fe2112b7251fe7ef33da6dac118886ae92f95622
Jason Nichols <jason@kickroot.com>
2017-01-09 16:42:47 -0500

    This one too

---
+++ config_command.go
@@ -0,0 +1,69 @@
+package main
+
+type configurationCommand struct{}
+
+func (c *configurationCommand) exec(tokens []string, term *Term, config *configuration) {
+	//
+	// A 'config' by itself just prompts for the current configuration
+	//
+	if len(tokens) == 1 {
+		term.printf("Current config: %s [%s]\n", config.name, config.path)
+		return
+	}
+
+	switch tokens[1] {
+	case "save":
+		// With only 2 params, save to the current config
+		targetConfig := currentConfig
+		if len(tokens) > 2 {
+			targetConfig = tokens[2]
+		}
+
+		configFile, err := getConfigPath(targetConfig)
+		if err != nil {
+			term.printf("Couldn't save %v: %v\n", targetConfig, err)
+			return
+		}
+
+		config.name = targetConfig
+		config.path = configFile
+		updatePrompt()
+		initCommands(config)
+		term.printf("Saving %v to %v\n", config.name, config.path)
+		err = config.writeConfig()
+		if err != nil {
+			term.printf("Couldn't save %v: %v\n", targetConfig, err)
+			return
+		}
+		updatePrompt()
+		initCommands(config)
+	case "list":
+		printConfigs(configRoot)
+	case "load":
+		if len(tokens) < 3 {
+			term.writeString("Please supply a configuration name as well, such as 'config load acro'\n")
+		} else {
+			configFile, err := getConfigPath(tokens[2])
+			if err != nil {
+				term.printf("Couldn't load %v: %v\n", tokens[2], err)
+				return
+			}
+			conf, err := loadConfig(tokens[2], configFile)
+			if err != nil {
+				term.printf("Couldn't load %v: %v\n", tokens[2], err)
+				return
+			}
+
+			// Todo: We need to encapsulate this better, these are defined in acro.go
+			config.name = conf.name
+			config.path = conf.path
+			config.settings = conf.settings
+			settings = &(conf.settings)
+			currentConfig = config.name
+			updatePrompt()
+			initCommands(config)
+		}
+	default:
+		term.printf("Unknown option '%s', try one of [save, list, load]\n", tokens[1])
+	}
+}
